383c65fcc394cd65845df51c4f238b4f85dbbcaa
Ar Kar <arkar20011@gmail.com>
2021-12-14 15:20:07 +0630

    header banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   content: ["./page/**/*.{html,js}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pandapink: "#d70f64",
+      },
+      fontFamily:{
+        open:['Open Sans']
+      }
+    },
   },
   plugins: [],
 };
