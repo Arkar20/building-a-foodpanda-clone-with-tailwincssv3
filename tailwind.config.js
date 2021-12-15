33af651494c405abd24a25023ebf1f9ffcd3cb5a
Ar Kar <arkar20011@gmail.com>
2021-12-15 22:50:42 +0630

    poster

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,14 @@ module.exports = {
       colors: {
         pandapink: "#d70f64",
       },
-      fontFamily:{
-        open:['Open Sans']
-      }
+      fontFamily: {
+        open: ["Open Sans"],
+      },
+      aspectRatio: {
+        "3/4":"3/4",
+        "16/9": "16 / 9",
+        "9/16": "9/16",
+      },
     },
   },
   plugins: [],
